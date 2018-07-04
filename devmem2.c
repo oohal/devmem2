0679fe8a4593b7b1021002c9e7865da506aff67f
Oliver O'Halloran <oohall@gmail.com>
2018-07-04 22:59:39 +1000

    query the page size via sysconf
    64bit powerpc and ARM both use a 64KB page size so we shouldn't hardcode
    4KB as the page size.

--- devmem2.c
+++ devmem2.c
@@ -42,14 +42,13 @@
 #define FATAL do { fprintf(stderr, "Error at line %d, file %s (%d) [%s]\n", \
   __LINE__, __FILE__, errno, strerror(errno)); exit(1); } while(0)
 
-#define MAP_SIZE 4096UL
-#define MAP_MASK (MAP_SIZE - 1)
 
 int main(int argc, char **argv)
 {
 	unsigned long read_result, writeval;
 	void *map_base, *virt_addr;
 	int access_type = 'w';
+	long page_size;
 	off_t target;
 	int fd;
 
@@ -73,14 +72,18 @@ int main(int argc, char **argv)
 	printf("/dev/mem opened.\n");
 	fflush(stdout);
 
-	/* Map one page */
-	map_base = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, target & ~MAP_MASK);
+
+	/* NB: The page size isn't always 4KB */
+	page_size = sysconf(_SC_PAGESIZE);
+
+	map_base = mmap(0, page_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd,
+			target & ~(page_size - 1));
 	if (map_base == MAP_FAILED)
 		FATAL;
 	printf("Memory mapped at address %p.\n", map_base);
 	fflush(stdout);
 
-	virt_addr = map_base + (target & MAP_MASK);
+	virt_addr = map_base + (target & (page_size - 1));
 	switch (access_type) {
 		case 'b':
 			read_result = *((unsigned char *) virt_addr);
@@ -122,7 +125,7 @@ int main(int argc, char **argv)
 		fflush(stdout);
 	}
 
-	if (munmap(map_base, MAP_SIZE) == -1)
+	if (munmap(map_base, page_size) == -1)
 		FATAL;
 
 	close(fd);
